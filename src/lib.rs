77e9fbdd72b8a78fcb30bf81c16c300be9b16fe6
yusuke.akimoto <yusuke.akimoto@ariseanalytics.com>
2024-10-04 10:47:54 +0000

    update documents

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! # Loggers
+//!
+//! ## Installation
+//! ```bash
+//! cargo add loggers
+//! ```
+//!
+//! # Examples
+//! ```rust
+//! use loggers::*;
+//! let mut logger = Logger::new();
+//! logger.add_logger(Box::new(CustomLogger::new(
+//!     "test",
+//!     "tests/output/system.log",
+//! )));
+//! logger.set_fallback(Box::new(CustomLogger::new(
+//!     "default",
+//!     "tests/output/system.log",
+//! )));
+//! log::set_boxed_logger(Box::new(logger)).expect("Failed to set logger");
+//! log::set_max_level(log::LevelFilter::Trace);
+//!
+//! info!(target:"test", "Hello, world!");
+//! debug!("Default");
+//! ```
+
 use chrono::{Local, SecondsFormat};
 use std::{fs::File, io::prelude::*, path::Path};
 pub struct Logger {
